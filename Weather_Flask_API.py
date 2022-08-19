48f26784c328262541f53fe9538225b3c0a385e9
Bobby Benedict <91276925+robertfben@users.noreply.github.com>
2022-08-19 11:10:31 -0400

    8-19-22

--- Weather_Flask_API.py
+++ Weather_Flask_API.py
@@ -44,7 +44,8 @@ def home():
             /api/v1.0/precipitation<br> \
             /api/v1.0/stations<br> \
             /api/v1.0/tobs<br> \
-            /api/v1.0/(start_date) and /api/v1.0/(start_date)/(end_date)"
+            /api/v1.0/(start_date) OR<br> \
+            /api/v1.0/(start_date)/(end_date)"
 
 ############ Next Route ##################
 
@@ -108,6 +109,85 @@ def stations():
 
 ############ Next Route ##################
 
+# Define when user hits temperature route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # create Session link from Python to DB
+    session = Session(engine)
+
+    # Perform a query to retrieve the dates and temperature observations of the most active station for the previous year of data
+    tobs_obs = session.execute("SELECT Measurement.date, Measurement.tobs FROM Measurement \
+                                        JOIN Station \
+                                        ON Measurement.station = Station.station \
+                                        WHERE Station.id = 7 \
+                                        AND Measurement.date between '2016-08-23' AND '2017-08-23';").all()
+    
+    session.close()
+
+    # create dictionary from the row data and append to a list
+    all_temps = []
+    for date, tobs in tobs_obs:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        all_temps.append(tobs_dict)
+    
+    # return jsonified list
+    return jsonify(all_temps)
+
+############ Next Route ##################
+
+# Define when user hits inputted start date route only
+@app.route("/api/v1.0/<start_date>")
+def Starting_Date_Temps(start_date):
+    """Fetch min, max, and avg temps. for all dates on and after given start date"""
+    # creating session link
+    session = Session(engine)
+
+    starting_date_minmaxAvg = session.execute(f"SELECT MIN(tobs), MAX(tobs), AVG(tobs) \
+                                                FROM Measurement \
+                                                WHERE date >= '{start_date}';").all()
+
+    session.close()
+
+    # create dictionary from the row data and append to list
+    minMaxAvg_List = []
+    for min, max, avg in starting_date_minmaxAvg:
+        minMaxAvg_Dict = {}
+        minMaxAvg_Dict["Minimum Temperature"] = min
+        minMaxAvg_Dict["Maximum Temperature"] = max
+        minMaxAvg_Dict["Average Temperature"] = avg
+        minMaxAvg_List.append(minMaxAvg_Dict)
+
+    # return jsonified list
+    return jsonify(minMaxAvg_List)
+
+############## Next Route ##################
+
+# Define when user hits inputted start date route only
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def Starting_Ending_Date_Temps(start_date,end_date):
+    """Fetch min, max, and avg temps. for all dates between given start date and end date"""
+    # creating session link
+    session = Session(engine)
+
+    starting_ending_date_minmaxAvg = session.execute(f"SELECT MIN(tobs), MAX(tobs), AVG(tobs) \
+                                                FROM Measurement \
+                                                WHERE date BETWEEN '{start_date}' AND '{end_date}';").all()
+
+    session.close()
+
+    # create dictionary from the row data and append to list
+    minMaxAvg_Between_List = []
+    for min, max, avg in starting_ending_date_minmaxAvg:
+        minMaxAvg_Between_Dict = {}
+        minMaxAvg_Between_Dict["Minimum Temperature"] = min
+        minMaxAvg_Between_Dict["Maximum Temperature"] = max
+        minMaxAvg_Between_Dict["Average Temperature"] = avg
+        minMaxAvg_Between_List.append(minMaxAvg_Between_Dict)
+
+    # return jsonified list
+    return jsonify(minMaxAvg_Between_List)
 
 
 ####################################################
